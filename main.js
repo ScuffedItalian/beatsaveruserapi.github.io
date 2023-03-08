a089428478d089feb98a4f6c9aae8306d1bb7d6c
ScuffedGuy <110801525+ScuffedItalian@users.noreply.github.com>
2023-03-07 23:44:47 -0800

    small changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const userInputID = document.getElementById("userINP");
 const button = document.getElementById("enterButton");
 
 
-button?.addEventListener("click", async () => {
+button.addEventListener("click", async () => {
     const userID = userInputID.value;
     const dataURL = fetch("https://api.beatsaver.com/users/id/${userID}")
     const response = await dataURL;

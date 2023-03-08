3cd0caefc06e0d0b5d26315f9bf1b9bec0af32ae
ScuffedGuy <110801525+ScuffedItalian@users.noreply.github.com>
2023-03-07 23:59:27 -0800

    OOOOO MORE CHANGES?!?!

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const button = document.getElementById("enterButton");
 
 button.addEventListener("click", async () => {
     const userID = userInputID.value;
-    const dataURL = fetch("https://api.beatsaver.com/users/id/107013")
+    const dataURL = fetch("https://api.beatsaver.com/search/text/0?noodle=true&sortOrder=Latest")
     const response = await dataURL;
     console.log(response);
 })
